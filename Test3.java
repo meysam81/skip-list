67ea114aa6bbac179c81b0090daeb697721e709c
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-02 07:20:03 +0330

    solution to new reqiurements

---
+++ Test3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Test3
+{
+   public static void main(String[] args)
+   {
+		Scanner scanner = new Scanner(System.in);
+		int qq = scanner.nextInt();
+		System.out.println(qq);
+   }
+}
